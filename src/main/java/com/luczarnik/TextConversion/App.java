adb33466cc0d793d784e564ae99d923356bfddcb
nova <257472@edu.p.lodz.pl>
2026-03-21 03:21:58 +0100

    project initialization

---
+++ src/main/java/com/luczarnik/TextConversion/App.java
@@ -0,0 +1,13 @@
+package com.luczarnik.TextConversion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
